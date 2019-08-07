7ef3db1c3c7dc5ebbe7353c892db2ea5c95d5285
Sai Srinivas Gorti <saisrinivas.gorti@ibm.com>
2019-08-07 15:30:38 -0500

    editing script files

--- Helm automations/helm_install_box.sh
+++ Helm automations/helm_install_box.sh
@@ -2,6 +2,6 @@
 
 cloudctl login -a https://10.0.42.112:8443 -u admin -p ibm@att -n default
 
-helm delete --purge box-att-deployment --tls
+helm install inference-repo/boxinference --tls --name box-att-deployment
 
 cloudctl logout
--- Helm automations/helm_install_hardhat.sh
+++ Helm automations/helm_install_hardhat.sh
@@ -4,6 +4,6 @@ cloudctl login -a https://10.0.42.112:8443 -u admin -p ibm@att -n default
 
 helm repo add inference-repo https://raw.githubusercontent.com/srinivas511/inference-server-repo/master
 
-helm install inference-repo/boxinference --tls --name box-att-deployment
+helm install inference-repo/inference --tls --name hardhat-att-deployment
 
 cloudctl logout
